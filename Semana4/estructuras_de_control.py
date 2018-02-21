236cdfb51f326e5d7e79bee07896d8171a35970d
jmcordoba@universidadean.edu.co <jmcordoba@universidadean.edu.co>
2018-02-20 19:14:39 -0500

    Agregado ejercicios de estructuras de control

--- Semana4/estructuras_de_control.py
+++ Semana4/estructuras_de_control.py
@@ -1,8 +1,37 @@
+def esPar(numero):
+    '''
+    (int) -> boolean
+
+    Valida si un numero es par
+
+    >>> esPar(2)
+    true
+    >>> esPar(9)
+    false
+
+    :param numero: el numero a verificar
+    :return: true si el numero es par false de lo contrario
+    '''
+    return numero%2==0
+
+def saludarHora(hora):
+    '''
+
+    :param hora:
+    :return:
+    '''
+
+
 numero = int(input("Digite su numero:\n"))
 
 print(numero)
 
-if(numero%2==0):
+
+#if nos permite tomar una decision
+if(esPar(numero)):
     print("El número "+ str(numero) + " es par")
+#else nos permite evaluar el caso contrario
 else:
     print("El número "+ str(numero) + " no es par")
+
+
