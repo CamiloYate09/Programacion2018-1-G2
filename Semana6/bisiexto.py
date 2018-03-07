4cee1914b6b0454e43969ffceb86b8adf14651ba
jmcordoba@universidadean.edu.co <jmcordoba@universidadean.edu.co>
2018-03-06 20:03:58 -0500

    Add Ejercicio Bisiexto

---
+++ Semana6/bisiexto.py
@@ -0,0 +1,28 @@
+def es_bisiexto(ano):
+    """
+    (int) -> boolean
+
+    Decide si un año es bisiexto
+
+    >>> es_bisiexto(4)
+    True
+    >>> es_bisiexto(100)
+    False
+    >>> es_bisiexto(400)
+    True
+
+    :param ano: el año a evaluar
+    :return: True si el año e bisiexto False de lo contrario
+    """
+    p = ano % 4 == 0
+    q = ano % 100 == 0
+    r = ano % 400 == 0
+    return p and (not q or r)
+
+
+ano_usuario = int(input("Ingrese su año\n"))
+
+if es_bisiexto(ano_usuario):
+    print("Su año es bisiexto\n")
+else:
+    print("Su año no es bisiexto\n")
