a3628cf37ce3684dfdaf8faa6060b7946379bc82
jmcordoba@universidadean.edu.co <jmcordoba@universidadean.edu.co>
2018-04-03 19:27:59 -0500

    Add Solución robot parlanchin

---
+++ Semana_8_5/robot_todo.py
@@ -0,0 +1,11 @@
+# Todo Definir el diccionario
+
+# Todo responder una pregunta del diccionario
+
+# Todo Si no sabe agregue la respuesta correcta
+
+# Todo repetir hasta que se despida
+
+# Todo limpiar la entrada de datos
+
+# Todo agregar soporte a archivos
